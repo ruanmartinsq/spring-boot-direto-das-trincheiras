8f99237d7dd42feef01310fe86cb58d202ed0b6e
ruanmartinsq <ruanlana34@gmail.com>
2026-08-03 17:30:03 -0300

    video03: initial commit of the project

---
+++ anime-service/src/test/java/academy/devdojo/anime_service/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.devdojo.anime_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
