91a2b0d4bc4683ed9e84d2793821ec6351521194
ruanmartinsq <ruanlana34@gmail.com>
2026-08-05 18:10:33 -0300

    video07: RequestMapping and Servlet Context

--- anime-service/src/main/java/academy/devdojo/controller/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("greetings")
+@RequestMapping("greetings")
 public class HelloController {
 
     @GetMapping("hi")
