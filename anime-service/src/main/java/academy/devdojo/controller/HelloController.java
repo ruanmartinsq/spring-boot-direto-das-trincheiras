14d24d5024ce7f4462082019534b7a1873a184fa
ruanmartinsq <ruanlana34@gmail.com>
2026-08-11 15:51:12 -0300

    video14: PostMapping

--- anime-service/src/main/java/academy/devdojo/controller/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controller/HelloController.java
@@ -1,14 +1,25 @@
 package academy.devdojo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
+@RequestMapping("v1/greetings")
 //@RequestMapping("greetings")
+@Slf4j // cria o objeto "log"
 public class HelloController {
 
-    @GetMapping("hi")
+    @GetMapping()
      public String hi() {
          return "Hello world";
+     }
+
+    @PostMapping
+    public Long save (@RequestBody String name) {
+        log.info("save '{}'", name);
+        return ThreadLocalRandom.current().nextLong(1, 1000); //Pega um gerador de números aleatórios adequado para uso em aplicações com múltiplas threads.
+
      }
 }
--- anime-service/src/main/java/academy/devdojo/domain/Anime.java
+++ anime-service/src/main/java/academy/devdojo/domain/Anime.java
@@ -1,18 +1,21 @@
 package academy.devdojo.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter //apenas adiciona os getters do id e name
+@AllArgsConstructor
 public class Anime {
     private Long id; //quando trabalha com ID geralmente usa Wrapper (Long)
     private String name;
 
-    public Anime(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    //@AllArgsConstructor faz isso
+    //public Anime(Long id, String name) {
+        //this.id = id;
+        //this.name = name;
+    //}
 
     public static List<Anime> getAnimes() {
         var naruto = new Anime(1L, "Naruto");
