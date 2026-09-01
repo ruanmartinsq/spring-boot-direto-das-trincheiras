432b78acd8945c47837d004012d625bc442971ff
ruanmartinsq <ruanlana34@gmail.com>
2026-09-01 18:05:59 -0300

    video34: unit tests 02

--- anime-service/src/test/java/academy/devdojo/repository/ProducerHardCodedRepositoryTest.java
+++ anime-service/src/test/java/academy/devdojo/repository/ProducerHardCodedRepositoryTest.java
@@ -4,6 +4,7 @@ import academy.devdojo.domain.Producer;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
@@ -35,16 +36,61 @@ class ProducerHardCodedRepositoryTest {
         }
     }
 
+    //@Order(1) ordem pra ser executado
     @Test
     @DisplayName("findAll returns a list with all producerList")
     void findAll_ReturnsAllProducers_Whensuccesfull() {
         //Quando vc receber/sentir um injectmock e algm chamou o producer data.geproducers eu qro que vc retorne os producerList
+        //Mockito, quando alguém chamar getProducers() nesse producerData falso, faça ele retornar producerList
         BDDMockito.when(producerData.getProducers()).thenReturn(producerList);
 
         var producers = repository.findAll();
 
         Assertions.assertThat(producers)
                 .isNotNull()
-                .hasSize(producerList.size());
+                .hasSameElementsAs(producerList);
+    }
+
+    @Test
+    @DisplayName("findById returns a producer with given id")
+    void findById_ReturnsAllProducersById_Whensuccesfull() {
+        //Quando vc receber/sentir um injectmock e algm chamou o producer data.geproducers eu qro que vc retorne os producerList
+        //Mockito, quando alguém chamar getProducers() nesse producerData falso, faça ele retornar producerList
+        BDDMockito.when(producerData.getProducers()).thenReturn(producerList);
+
+        var expectedProducer = producerList.getFirst();
+        var producers = repository.findById(expectedProducer.getId());
+
+        Assertions.assertThat(producers)
+                .isPresent()
+                .contains(expectedProducer);
+    }
+
+    @Test
+    @DisplayName("findByName returns empty list when name is null")
+    void findByName_ReturnsEmptyList_WhenNameIsNull() {
+        //Quando vc receber/sentir um injectmock e algm chamou o producer data.geproducers eu qro que vc retorne os producerList
+        //Mockito, quando alguém chamar getProducers() nesse producerData falso, faça ele retornar producerList
+        BDDMockito.when(producerData.getProducers()).thenReturn(producerList);
+
+        var producers = repository.findByName(null);
+        Assertions.assertThat(producers)
+                .isNotNull()
+                .isEmpty();
+    }
+
+    @Test
+    @DisplayName("findByName returns list with found object when name exists")
+    void findByName_ReturnsFoundProducer_WhenNameIsFound() {
+        //Quando vc receber/sentir um injectmock e algm chamou o producer data.geproducers eu qro que vc retorne os producerList
+        //Mockito, quando alguém chamar getProducers() nesse producerData falso, faça ele retornar producerList
+        BDDMockito.when(producerData.getProducers()).thenReturn(producerList);
+
+        var expectedProducer = producerList.getFirst();
+        var producers = repository.findByName(expectedProducer.getName());
+
+        Assertions.assertThat(producers)
+                .isNotNull()
+                .contains(expectedProducer);
     }
 }
